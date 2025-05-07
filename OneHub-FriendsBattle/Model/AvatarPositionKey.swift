82a5f56713690b176054dd9fa640770777a644f4
giorgi3361 <giorgilakisli@gmail.com>
2025-05-07 06:49:46 -0700

    delete

--- OneHub-FriendsBattle/Model/AvatarPositionKey.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct AvatarPositionKey: PreferenceKey {
-    static var defaultValue: CGRect = .zero
-
-    static func reduce(value: inout CGRect, nextValue: () -> CGRect) {
-        value = nextValue()
-    }
-}
--- Pods/Target Support Files/Pods-OneHub-FriendsBattle/Pods-OneHub-FriendsBattle-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OneHub_FriendsBattle : NSObject
-@end
-@implementation PodsDummy_Pods_OneHub_FriendsBattle
-@end
